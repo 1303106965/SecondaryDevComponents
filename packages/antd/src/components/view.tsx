469b88d15122f40252789e9987333bc44edb1538
Yangzh <VioletEkko@proton.me>
2024-11-11 16:00:13 +0800

    删除antd组件

--- packages/antd/src/components/view.tsx
+++ packages/antd/src/components/view.tsx
@@ -1,33 +1,10 @@
 import React from 'react';
 import cx from 'classnames';
-import { Box, BoxProps } from './box';
+import { View as androidView } from 'react-native';
 import { defineComponent } from '@music163/tango-boot';
 import { Placeholder } from './placeholder';
 
-export interface ViewProps extends Omit<BoxProps, 'title'> {
-  title?: React.ReactNode;
-}
-
-function AndroidView({ title, className, children, ...rest }: ViewProps) {
-  return (
-    <Box
-      as="section"
-      className={cx('td-section', className)}
-      bg="#FFF"
-      borderRadius="2px"
-      {...rest}
-    >
-      {title && (
-        <Box p="24px 24px 0" fontSize="16px" fontWeight="500">
-          {title}
-        </Box>
-      )}
-      <Box p="24px">{children}</Box>
-    </Box>
-  );
-}
-
-export const View = defineComponent(AndroidView, {
+export const View = defineComponent(androidView, {
   designerConfig: {
     render({ children, designerProps, originalProps }) {
       return React.cloneElement(children, {
